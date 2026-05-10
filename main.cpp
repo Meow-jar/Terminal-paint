236a5ba4dabc5f27da620f73185bbf7d318c3897
Meow-jar <nikitin1991.10@gmail.com>
2026-05-10 12:26:41 +0300

    build 3

--- main.cpp
+++ main.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
+
+//широта и высота 
+const int width = 120;
+const int height = 30;
+
+//размер символов в пикселях 
+const float pixelasets = 11.0f / 24.0f;
+    
+//функция для вывода круга
+float circle(float& x, float& y, int& i, int& j,char* win)
+{
+    char pixel = ' ';
+    if(x * x + y * y < 0.7) pixel = '@';
+    win[i + j * width] = pixel; 
+    return 0;
+}
+
 int main()
 {
-    //широта и высота 
-    const int width = 120;
-    const int height = 30;
-
-    //размер символов в пикселях 
-    const float pixelasets = 11.0f / 24.0f;
-    
     //переменная для хранения символов - пикселей
     char* win = new char[width * height + 1];
     win[width * height] = '\0';
-    
-        for (int i = 0; i < width; i++)
+
+    int i = 0, j = 0;
+        for (i = 0; i < width; i++)
         {
-            for(int j = 0; j < height; j++)
+            for(j = 0; j < height; j++)
             {  
                 
                 float x = (float)i / width * 2.0f - 1.0f;
@@ -22,19 +33,15 @@ int main()
                 //выравнивание к соотнашению 1 к 1
                 float aspect = (float)width / height;
                 x *= aspect * pixelasets;
-                //прямоугольник
+
                 char pixel = ' ';
-                if(x * x <= 0.5) pixel = '@';            
-                win[i + j * width] = pixel;    
-                //круг
-                /*
-                if(x * x + y * y > 0.7) pixel = '@';
-                win[i + j * width] = pixel;
-                */
+                circle(x,y,i,j,win);
+
+                
             }
         }
-
-    printf(win);
+    
+    printf("%s" , win);
     delete [] win;
     std::cin.get();
     return 0;
