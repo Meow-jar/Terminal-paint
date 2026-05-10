c95eb82282680bd1a6658a7353b9bdf25d4f2c9e
Meow-jar <nikitin1991.10@gmail.com>
2026-05-10 12:42:17 +0300

    build 3.2

--- main.cpp
+++ main.cpp
@@ -1,39 +1,39 @@
 #include <iostream>
 
 //широта и высота 
-const int width = 120;
-const int height = 30;
+const int Width = 120;
+const int Height = 30;
 
 //размер символов в пикселях 
-const float pixelasets = 11.0f / 24.0f;
+const float PixelAssets = 11.0f / 24.0f;
     
 //функция для вывода круга
 void circle(float& x, float& y, int& i, int& j,char* win)
 {
-    char pixel = ' ';
-    if(x * x + y * y < 0.7) pixel = '@';
-    win[i + j * width] = pixel; 
+    char Pixel = ' ';
+    if(x * x + y * y < 0.7) Pixel = '@';
+    win[i + j * Width] = Pixel; 
 }
 
 int main()
 {
     //переменная для хранения символов - пикселей
-    char* win = new char[width * height + 1];
-    win[width * height] = '\0';
+    char* win = new char[Width * Height + 1];
+    win[Width * Height] = '\0';
 
     int i = 0, j = 0;
-        for (i = 0; i < width; i++)
+        for (i = 0; i < Width; i++)
         {
-            for(j = 0; j < height; j++)
+            for(j = 0; j < Height; j++)
             {  
                 
-                float x = (float)i / width * 2.0f - 1.0f;
-                float y = (float)j / height * 2.0f - 1.0f;
+                float x = (float)i / Width * 2.0f - 1.0f;
+                float y = (float)j / Height * 2.0f - 1.0f;
                 //выравнивание к соотнашению 1 к 1
-                float aspect = (float)width / height;
-                x *= aspect * pixelasets;
+                float Aspect = (float)Width / Height;
+                x *= Aspect * PixelAssets;
 
-                char pixel = ' ';
+                char Pixel = ' ';
                 circle(x,y,i,j,win);
 
                 
